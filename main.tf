59cbe324c84783d4d6cab3709b9053d3adab9604
Gaurvssris <gauravsrivastwa85@gmail.com>
2025-07-29 14:10:48 +0530

    newrsg

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.37.0"
+    }
+  }
+}
+
+provider "azurerm" {
+    features{}
+    subscription_id = "661ea49e-05d4-429e-8204-de0d46c539b5"
+  # Configuration options
+}
+resource "azurerm_resource_group" "gauravssris2" {
+  name = "gauravssris2"
+  location = "central india"
+  
+}
